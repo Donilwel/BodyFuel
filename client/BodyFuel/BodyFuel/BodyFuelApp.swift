cd58be08ef737efc332a82a134c1e16a8eda42a3
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2026-01-19 19:14:23 +0300

    init

---
+++ client/BodyFuel/BodyFuel/BodyFuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyFuelApp.swift
+//  BodyFuel
+//
+//  Created by Дарья Дробышева on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BodyFuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
