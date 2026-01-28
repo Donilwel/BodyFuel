07af07d705a78a961c3055be2735284b39a4e7ca
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2026-01-28 18:23:34 +0300

    feat: change endpoints due to server changes

--- client/BodyFuel/Services/Network/NetworkConstants.swift
+++ client/BodyFuel/Services/Network/NetworkConstants.swift
@@ -3,8 +3,8 @@ import Foundation
 enum API {
     static let baseURLString = "http://172.20.10.12:8080/api/v1"
     
-    static let userParameters = "/crud/user/params"
-    static let weight = "/crud/user/weight"
+    static let userParameters = "/user/params"
+    static let weight = "/user/weight"
     static let uploadAvatar = "/avatars"
     
     enum Auth {
