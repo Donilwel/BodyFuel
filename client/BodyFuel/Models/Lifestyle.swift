6d44843bfdf88aaeb6a9080f141c781e41c6eac5
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2026-01-28 03:02:21 +0300

    fix: fix lifestyle

--- client/BodyFuel/Models/Lifestyle.swift
+++ client/BodyFuel/Models/Lifestyle.swift
@@ -1,13 +1,31 @@
 enum Lifestyle: String, CaseIterable, Identifiable {
-    case active
     case sedentary
+    case active
+    case sporty
 
     var id: String { rawValue }
 
     var title: String {
         switch self {
-        case .active: return "Активный"
         case .sedentary: return "Малоподвижный"
+        case .active: return "Умеренная активность"
+        case .sporty: return "Очень высокая активность"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .sedentary: return "Сидячая работа, мало или нет тренировок"
+        case .active: return "Умеренные тренировки 3-5 раз в неделю"
+        case .sporty: return "Очень интенсивные тренировки или тяжелая физическая работа"
+        }
+    }
+    
+    var physicalActivityLevel: Float {
+        switch self {
+        case .sedentary: return 1.2
+        case .active: return 1.55
+        case .sporty: return 1.9
         }
     }
 }
